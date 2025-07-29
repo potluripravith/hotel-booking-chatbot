ff22f4fb4879be1ff1f3ab99d4b164cb819458bc
potluripravith <112188413+potluripravith@users.noreply.github.com>
2025-07-29 12:27:01 +0530

    utils_faq.py is completely updated

--- utils/faq_utils.py
+++ utils/faq_utils.py
@@ -1,17 +1,54 @@
 import pandas as pd
-from difflib import get_close_matches
+from rapidfuzz import fuzz, process
 import os
-def load_faq_data():
-    base_dir = os.path.dirname(os.path.abspath(__file__))  # utils/
+import re
+import string
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.data import find
+
+def ensure_nltk_resources():
+    """Ensures that necessary NLTK resources ('punkt' and 'stopwords') are available and Downloads them if not already present"""
+    for resource_id, resource_name in [("tokenizers/punkt", "punkt"), ("corpora/stopwords", "stopwords")]:
+        try:
+            find(resource_id)
+        except LookupError:
+            nltk.download(resource_name)
+
+ensure_nltk_resources()
+
+STOP_WORDS = set(stopwords.words('english'))
+
+def clean_text(text: str) -> str:
+    """Cleans input text by lowercasing, removing punctuation, tokenizing and filtering out English stopwords."""
+    text = text.lower()
+    text = re.sub(f"[{string.punctuation}]", "", text)
+    tokens = word_tokenize(text)
+    filtered_tokens = [word for word in tokens if word not in STOP_WORDS]
+    return " ".join(filtered_tokens)
+
+def load_faq_data()-> pd.DataFrame:
+    """Loads FAQ data from a CSV file located in the 'knowledge_base' directory"""
+    base_dir = os.path.dirname(os.path.abspath(__file__))
     path = os.path.join(base_dir, '..', 'knowledge_base', 'Hotel_faqs.csv')
-    df = pd.read_csv(path)
-    return df
-def get_faq_answer(user_question, faq_df):
-    questions = faq_df["question"].tolist()
-    matches = get_close_matches(user_question, questions, n=1, cutoff=0.5754)
-    if matches:
-        matched_question = matches[0]
-        answer_row = faq_df[faq_df["question"] == matched_question]
-        return answer_row["answer"].values[0]
-    else:
-        return "Sorry, I couldn't find an answer to that question."
+    return pd.read_csv(path)
+
+def get_faq_answer(user_question: str, faq_df: pd.DataFrame) -> str:
+    """Finds the most relevant FAQ answer to the user's question using fuzzy string matching"""
+    if "question" not in faq_df.columns or "answer" not in faq_df.columns:
+        raise ValueError("FAQ CSV must contain 'question' and 'answer' columns.")
+    cleaned_user_question = clean_text(user_question)
+
+    cleaned_questions = faq_df["question"].apply(clean_text)
+    match = process.extractOne(
+    cleaned_user_question,
+    cleaned_questions,
+    scorer=fuzz.token_sort_ratio
+    )
+    FUZZY_MATCH_THRESHOLD = 55
+    if match and match[1] >= FUZZY_MATCH_THRESHOLD:
+        _, _, index = match
+        return faq_df.loc[index, "answer"]
+
+    return "Sorry, I couldn't find an answer to that question."
